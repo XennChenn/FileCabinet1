b499b8b060ace47e2495a712670bda8d42bcfa00
Chenn <bxc_chenn@hotmail.com>
2016-08-27 09:53:32 +0800

    add controller posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,26 @@
 class PostsController < ApplicationController
   def index
+  	@posts = Post.all
   end
 
   def show
   end
 
   def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def set_post
+  	@post = Post.find(params[:id])
+  end
+
+  def post_params
+  	params.require(:post).permit(:title, :content)
   end
 end
