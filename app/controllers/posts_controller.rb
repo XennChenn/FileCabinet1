1c15bde5392b85d0f5047f20e32635a0ef89f39e
Chenn <bxc_chenn@hotmail.com>
2016-08-28 16:13:31 +0800

    done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,44 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   def index
-  	@posts = Post.all
+  	@posts = Post.where(user_id: current_user).order('created_at DESC')
   end
 
   def show
+  end
+
+  def new
+  	@post = current_user.posts.build
+  end
+
+  def create
+  	@post = current_user.posts.build(post_params)
+
+  	if @post.save
+  		redirect_to posts_path
+  	else
+  		render :new
+  	end
   end
 
   def edit
   end
 
   def update
+  	@post.update(post_params)
+
+  	if @post.save
+  		redirect_to @post
+  	else
+  		render :edit
+  	end
   end
 
   def destroy
+  	@post.destroy
+  	redirect_to posts_path
   end
 
   private
